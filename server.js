c828fec8c59768a11fea27d9d0c7b3d4a2bf5ae3
Rahul <lrahul949@gmail.com>
2017-07-17 14:35:08 +0530

    making changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require('express'),
 app.use(express.static(__dirname + '/client'));
 
 app.get('/', function(req,res){
-	res.sendFile(__dirname + '/client/views/index.html');
+	res.sendFile(__dirname + '/client/views/home.html');
 });
 
 app.listen(port, function(){
